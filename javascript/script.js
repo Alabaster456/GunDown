7531eb34e4bca43d3e6af5ac845a7924364ae933
Yazid <Alabaster456@users.noreply.github.com>
2014-03-04 17:31:50 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,12 @@
-k
+//Variables
+
+var userclick = true; //If this is true, it will automatically contribute to the user's action in winning.
+//END
+
+
+
+//OPPONENT
+var opponent = function(speed) {
+  this.speed = speed; 
+}
+
