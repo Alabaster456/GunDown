ad6ece023ce3fbc7fb4f9e3aefe012ce2983a3e2
Yazid <Alabaster456@users.noreply.github.com>
2014-03-05 12:09:23 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,31 @@
 //Variables
-
 var userclick = true; //If this is true, it will automatically contribute to the user's action in winning.
 //END
 
+//HUMAN OBJECT (PLAYER)
+var human = {
+  name: find;
+  };
 
+//Finds the user's name
+function find(name) {
+  return name;
+}
 
-//OPPONENT
-var opponent = function(speed) {
+//VARIETY OF OPPONENTS
+var opponent = function(name, speed) { 
+  this.name = name; 
   this.speed = speed; 
 }
 
+//Creates multiple opponents with the custom constructor 'opponent'.
+var Steven = new opponent("Steven", 0.25);
+var Chuckie = new opponent("Chuckie", 0.56);
+//END
+
+
+if (!userclick) {
+  console.log("You lost," + " " + human.name);
+} else {
+  console.log("You won," + " " + human.name);
+}
