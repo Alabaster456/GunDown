b6c4acaeb46140766096c42b1479b06916a48eb7
Yazid <Alabaster456@users.noreply.github.com>
2014-03-09 14:42:08 -0500

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -3,17 +3,25 @@
 /* alert("Welcome to GunDown!");
 var charselect = function(select) {
  alert("Choose yer' character!");
-  $(document).ready(function() { $('#starz').css("display", "initial");
+  $(document).ready(function() {
+    $('#rhino').css("display", "initial");
+ $('#starz').css("display", "initial");
      $('#bugger').css("display", "initial");
 $('#starz').click(function(){
-alert("STARZ iz mah' name! Yer' gonna win, sir!");
+alert("'STARZ iz mah' name! Yer' gonna win, sir!'");
+  $('#bugger').remove();
+  $('#rhino').remove();
+  alert("You chose Starz!");
+}), $('#bugger').click(function(){
+  alert("'Oi! Am Bugger! I'm an pure outlar', but I get round' dese' parts!'");
+   $('#starz').remove();
+  $('#rhino').remove();
+  alert("You chose bugger!");
+}), $('#rhino').click(function() {
+    alert("'Da' nam' is Rhino. I'm a mysterious, walking little fella who knows how to shoot.'"); 
   $('#starz').remove();
   $('#bugger').remove();
-  return;
-}), $('#bugger').click(function(){
-  alert("Oi! Am Bugger! I'm an pure outlar', but I get round' dese' parts!");
-   $('#starz').remove();
-  $('#bugger').remove();
+  alert("You chose Rhino!");
 });
 });
 };
