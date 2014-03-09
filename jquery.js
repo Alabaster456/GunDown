e8ff84ef358fb204b681085d864ef0e25ecbca87
Yazid <Alabaster456@users.noreply.github.com>
2014-03-09 13:54:52 -0500

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,2 +1,17 @@
-$(document).ready() {
-}
+var charselect = function(select) {
+ alert("Choose yer' character!");
+ $(document).ready(function() {
+$('#starz').click(function(){
+alert("STARZ iz mah' name! Yer' gonna win, sir!");
+  $('#starz').remove();
+  $('#bugger').remove();
+  return;
+}), $('#bugger').click(function(){
+  alert("Oi! Am Bugger! I'm an pure outlar', but I get round' dese' parts!");
+   $('#starz').remove();
+  $('#bugger').remove();
+});
+});
+};
+alert("Welcome to GunDown!");
+charselect();
