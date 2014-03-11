af5f0c4c8b618c3570dbac9447f842b6e0355a5c
Yazid <Alabaster456@users.noreply.github.com>
2014-03-10 19:14:01 -0500

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -18,26 +18,35 @@ alert("'Starz iz mah' name! Yer' gonna win, sir!'");
    $('#starz').remove();
   $('#rhino').remove();
   alert("You chose bugger!");
+  alert("Your opponent is..");
+  findenemy();
+  
 }), $('#rhino').click(function() {
-    alert("'Da' nam' is Rhino. I'm a mysterious, walking little fella who knows how to shoot.'"); 
+    alert("'Da' nam' is Rhino. I'm a mysterious, walking lil' fella who knows how to shoot.'"); 
   $('#starz').remove();
   $('#bugger').remove();
   alert("You chose Rhino!");
-});
-});
-};
-var findenemy = function(search) {
+  alert("Your opponent is..");
+  findenemy();
+  
+//FUNCTION INSIDE THE DOCUMENT
+var findenemy = function() {
   var starz = Math.floor(Math.random()*3+1);
 var rhino = Math.floor(Math.random()*3+1);
 var bugger = Math.floor(Math.random()*3+1);
   if (starz === 1) {
-      alert("You're goin' against Starz!");
+      alert("You're goin' against Starz!"); $('#starz2').css("display", "initial");
+
+    
   } else if (rhino === 2) {
       alert("You're goin' against Rhino!");
   } else {
       alert("You're goin' against Bugger!");
-$('#bugger').css("display", "initial");
   }
+};
+  
+});
+});
 };
 charselect();
 
